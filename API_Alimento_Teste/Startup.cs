7b4488e602e84f9f472faa4af39065def2812938
Mateus Moraes <mtu_h@hotmail.com>
2021-08-18 17:36:59 -0300

    Frontend

--- API_Alimento_Teste/Startup.cs
+++ API_Alimento_Teste/Startup.cs
@@ -2,18 +2,11 @@ using API_Alimento_Teste.DA;
 using API_Alimento_Teste.Lib.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace API_Alimento_Teste
 {
@@ -39,12 +32,12 @@ namespace API_Alimento_Teste
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API_Alimento_Teste", Version = "v1" });
             });
 
-            //services.AddCors(o => o.AddPolicy("AllowFull", builder =>
-            //  {
-            //      builder.AllowAnyMethod()
-            //             .AllowAnyHeader()
-            //             .AllowAnyOrigin();
-            //  }));
+            services.AddCors(o => o.AddPolicy("AllowFull", builder =>
+              {
+                  builder.AllowAnyMethod()
+                         .AllowAnyHeader()
+                         .AllowAnyOrigin();
+              }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
