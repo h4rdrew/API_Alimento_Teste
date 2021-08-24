4fb6a8094fea21e917c5e4606fc416c3c4f7767d
Mateus Moraes <mtu_h@hotmail.com>
2021-08-24 17:59:39 -0300

    AtualizarCadastro(); agora utilizando Material

--- Frontend/FrontAlimento/src/app/app.module.ts
+++ Frontend/FrontAlimento/src/app/app.module.ts
@@ -1,21 +1,33 @@
 import { AlimentoCadastroComponent } from './alimentos/cadastro/alimento-cadastro/alimento-cadastro.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatTableModule} from '@angular/material/table';
 @NgModule({
   declarations: [
     AppComponent,
     AlimentoCadastroComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatInputModule,
+    MatTabsModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
